ea5752506b2aa3e81bb5ce2404c42e3a168775af
Thomas Theron <20700601@sun.ac.za>
2020-03-12 11:47:15 +0200

    infinite while for stage getting

--- src/mokse.py
+++ src/mokse.py
@@ -1,15 +1,18 @@
 import eskomStatus
 import mokse_led.py
+import time
 
 eskom_url = "http://loadshedding.eskom.co.za"
 status_loc = "/LoadShedding/GetStatus"
 url_glob = eskom_url + status_loc
 
-if eskomStatus.getStatus(url_glob) == 200:
-    print("Successful response 200")
-    #print(eskomStatus.getLevel(url_glob))
-    stage = eskomStatus.getLevel(url_glob)
-    shine(stage)
+while(1):
+    if eskomStatus.getStatus(url_glob) == 200:
+        print("Successful response 200")
+        #print(eskomStatus.getLevel(url_glob))
+        stage = eskomStatus.getLevel(url_glob)
+        shine(stage-1)
 
-else:
-    print("Error")
+    else:
+        print("Error")
+    time.sleep(1)
--- src/mokse_led.py
+++ src/mokse_led.py
@@ -1,19 +1,40 @@
 import RPi.GPIO as io
 import time
 
+
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+
+
 def shine(stage):
     # create matrix device
     serial = spi(port=0, device=0, gpio=noop())
-    device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation,
-                     rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
-    print("Created device")
+    device = max7219(serial, cascaded=1, block_orientation=0,
+                     rotate=0, blocks_arranged_in_reverse_order=0)
+    #print("Created device")
 
     # start demo
-    msg = "MAX7219 LED Matrix Demo"
+    #msg = "MAX7219 LED Matrix Demo"
+    msg =str(stage-1) 
     print(msg)
     show_message(device, msg, fill="white", font=proportional(CP437_FONT))
     time.sleep(1)
-
+    while(1):
+#        print(msg)
+#        show_message(device, msg, fill="white", font=proportional(CP437_FONT))
+#        time.sleep(1)
+        time.sleep(1)
+        with canvas(device) as draw:
+            text(draw, (0, 0), msg, fill="white")
+        time.sleep(1)
+        for _ in range(5):
+            for intensity in range(16):
+                device.contrast(intensity * 16)
+                time.sleep(0.1)
 #    stages[stage]()
 """ 
 stages = {
